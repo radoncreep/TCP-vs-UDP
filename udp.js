d8ea41a3a936b1168f518477bff029963085175d
Victor Onofiok <radoncreep@gmail.com>
2020-07-21 16:42:57 +0100

    tc/udp

---
+++ udp.js
@@ -0,0 +1,11 @@
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4'); // creating socket connection with udp4
+
+socket.on('message', (msg, rinfo) => {
+    // receiving msg req from remote address and port
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+socket.bind(8081);
+
+
